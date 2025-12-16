69ec276ea33feaf7a7071569d08b534477baaaa2
Yukinoshita <nasazy@gmail.com>
2025-12-16 02:57:29 +0000

    update promotion deletion.

--- delete_promotion.sh
+++ delete_promotion.sh
@@ -2,18 +2,17 @@
 
 #v2rayN/v2rayN/Views/MainWindow.xaml.cs
 sed -i '/menuPromotion\.Click/d' v2rayN/v2rayN/Views/MainWindow.xaml.cs
-line=$(sed -n '/void menuPromotion_Click/=' v2rayN/v2rayN/Views/MainWindow.xaml.cs)
+line=$(sed -n '/void MenuPromotion_Click/=' v2rayN/v2rayN/Views/MainWindow.xaml.cs)
 sed -i "$((line-0)),$((line+4))d" v2rayN/v2rayN/Views/MainWindow.xaml.cs
 
 #v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
 sed -i '/menuPromotion\.Click/d' v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
-line=$(sed -n '/void menuPromotion_Click/=' v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs)
+line=$(sed -n '/void MenuPromotion_Click/=' v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs)
 sed -i "$((line-0)),$((line+4))d" v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
 
 #v2rayN/v2rayN/Views/MainWindow.xaml
-line=$(sed -n '/ResUI.menuPromotion/=' v2rayN/v2rayN/Views/MainWindow.xaml)
-sed -i "$((line-8)),$((line+5))d" v2rayN/v2rayN/Views/MainWindow.xaml
+line=$(sed -n '/x:Name.*menuPromotion/=' v2rayN/v2rayN/Views/MainWindow.xaml)
+sed -i "$((line-2)),$((line+13))d" v2rayN/v2rayN/Views/MainWindow.xaml
 
 #v2rayN/v2rayN.Desktop/Views/MainWindow.axaml
-line=$(sed -n '/ResUI.menuPromotion/=' v2rayN/v2rayN.Desktop/Views/MainWindow.axaml)
-sed -i "$((line-3)),$((line+4))d" v2rayN/v2rayN.Desktop/Views/MainWindow.axaml
+sed -i '/ResUI\.menuPromotion/d' v2rayN/v2rayN.Desktop/Views/MainWindow.axaml
