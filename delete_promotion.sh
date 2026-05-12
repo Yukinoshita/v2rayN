25da6247b624d9b0145f17f32c00dff75549427b
Yukinoshita <nasazy@gmail.com>
2026-05-12 10:00:44 +0800

    Refactor delete_promotion.sh to use sed directly
    Removed the custom function for deleting promotion-related code and replaced it with sed commands for visibility adjustment.

--- delete_promotion.sh
+++ delete_promotion.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
 
-uni_sed(){
-  sed "$1" "$2" > "$2".temp
-  mv "$2".temp "$2"
-}
-
-#v2rayN/v2rayN/Views/MainWindow.xaml.cs
-uni_sed '/menuPromotion\.Click/d' v2rayN/v2rayN/Views/MainWindow.xaml.cs
-line=$(sed -n '/void MenuPromotion_Click/=' v2rayN/v2rayN/Views/MainWindow.xaml.cs)
-uni_sed "$((line-0)),$((line+4))d" v2rayN/v2rayN/Views/MainWindow.xaml.cs
-
-#v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
-uni_sed '/menuPromotion\.Click/d' v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
-line=$(sed -n '/void MenuPromotion_Click/=' v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs)
-uni_sed "$((line-0)),$((line+4))d" v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
-
-#v2rayN/v2rayN/Views/MainWindow.xaml
-line=$(sed -n '/x:Name.*menuPromotion/=' v2rayN/v2rayN/Views/MainWindow.xaml)
-uni_sed "$((line-2)),$((line+13))d" v2rayN/v2rayN/Views/MainWindow.xaml
-
-#v2rayN/v2rayN.Desktop/Views/MainWindow.axaml
-uni_sed '/ResUI\.menuPromotion/d' v2rayN/v2rayN.Desktop/Views/MainWindow.axaml
+sed -i 's/x:Name="menuPromotion"/x:Name="menuPromotion" Visibility="Collapsed"/g' v2rayN/Views/MainWindow.xaml
+sed -i 's/x:Name="menuPromotion"/x:Name="menuPromotion" Visibility="Collapsed"/g' v2rayN/v2rayN.Desktop/Views/MainWindow.axaml
